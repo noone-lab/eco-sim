9a71c35dab9858ee37f91c3a8b659175af509d6d
noone-lab <pakhalepranav16@gmail.com>
2025-09-18 03:45:08 +0530

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,4 @@
+import React from "https://esm.sh/react";
+import ReactDOM from "https://esm.sh/react-dom/client";
+import App from "./App.jsx";
+ReactDOM.createRoot(document.getElementById("root")).render(React.createElement(App));
